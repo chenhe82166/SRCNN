5eb85b8d36cb791936df7aa30c648e7328cc441b
chenhe82166 <32590367+chenhe82166@users.noreply.github.com>
2017-12-13 15:22:20 +0800

    Update minimum.py

--- Super_resolution-master/networks/minimum.py
+++ Super_resolution-master/networks/minimum.py
@@ -1,18 +1,18 @@
 from keras.models import Sequential
-from keras.layers import Activation, Dense
 from keras.layers import Conv2D
+from keras.optimizers import Adam,SGD
 
 
 def build_model(input_size):
-	input_shape = (input_size[0], input_size[1], 3)
+    input_shape = (input_size[0], input_size[1], 3)
     model = Sequential()
-    model.add(Conv2D(filters=32, kernel_size=(3, 3), activation='relu',
-					input_shape=input_shape, padding='same'))
-    model.add(Conv2D(filters=16, kernel_size=(5, 5), activation='relu',
-					padding='same'))
-    model.add(Conv2D(filters=3, kernel_size=(3, 3), activation='relu',
-					padding='sigmoid'))
-	optimizer = Adam(lr=0.001)
-	# optimizer = SGD(lr=0.01, momentum=0.9)
-	model.compile(optimizer=optimizer,loss='mse')
+    model.add(Conv2D(filters=32, kernel_size=(3, 3), activation='relu',init='glorot_uniform',
+					padding='same',use_bias=True,input_shape=input_shape))
+    model.add(Conv2D(filters=16, kernel_size=(5, 5), activation='relu',init='glorot_uniform',
+					padding='same',use_bias=True))
+    model.add(Conv2D(filters=3, kernel_size=(3, 3), activation='sigmoid',init='glorot_uniform',
+					padding='same',use_bias=True))
+    optimizer = Adam(lr=0.001)
+    # optimizer = SGD(lr=0.01, momentum=0.9)
+    model.compile(optimizer=optimizer,loss='mse')
     return model
